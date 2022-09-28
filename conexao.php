2b5d2490b72b98453fda34a45c16595afc4f4417
WagCar <wagner.carlin@hotmail.com>
2022-09-28 20:37:41 -0300

    Apos as mudanças e criando o arquivo autenticar

--- conexao.php
+++ conexao.php
@@ -6,8 +6,8 @@ date_default_timezone_set('America/Sao_Paulo');
 try{
 
 $pdo = new PDO("mysql:dbname=$banco; host=$servidor; charset=utf8", "$usuario", "$senha");
-} catch(Exception $e) {
-    echo "Não foi possível a conexão com o banco! <br> <br> " $e;
+} catch (Exception $e) {
+    echo 'ERRO: Não foi possível conectar ao Banco de Dados! <br> <br>'.$e;
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php 
-require_once("config.php");
+require_once("conexao.php");
 //CRIANDO O USUARIO ADMINISTRADOR CASO NÃO EXISTA!
+$query = $pdo->query("SELECT * FROM usuarios WHERE nivel = 'administrador'");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$total_reg = @count($res);
 
-$pdo->query("INSERT INTO usuarios SET nome = '$nome_admin', email='$email_admin' , senha = '$senha_admin' , nivel = 'Administrador',genero = '$genero' ");    
+if($total_reg == 0){ 
+    $pdo->query("INSERT INTO usuarios SET nome   = '$nome_admin ', email='$email_admin',senha='$senha_admin', nivel='Administrador', genero='$genero'");    
+}
 
 ?>
 
