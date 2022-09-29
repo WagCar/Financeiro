9d5bd1d19408faf6583e366e944624c106974496
WagCar <wagner.carlin@hotmail.com>
2022-09-29 14:55:02 -0300

    Alterações aula 17 em diante - 29/09

--- autenticar.php
+++ autenticar.php
@@ -1,4 +1,5 @@
 <?php 
+@session_start();
 require_once("conexao.php");
 $email = $_POST['email'];
 $senha = $_POST['senha'];
@@ -11,5 +12,21 @@ $res = $query->fetchAll(PDO::FETCH_ASSOC);
 $total_reg = @count($res);
 
 echo $total_reg;
+
+if ($total_reg > 0) {
+    $nivel = $res[0]['nivel'];
+
+    //variaveis de sessão
+    $_SESSION['nivel_usuario'] = $res[0]['nivel'];
+    $_SESSION['id_usuario']    = $res[0]['id'];   
+    $_SESSION['nome_usuario']  = $res[0]['nome'];
+
+    if ($nivel =='administrador') {    
+    echo "<script> window.location='painel-adm'</script>";
+    }
+}else {
+    echo "<script> window.alert('Dados Incorretos!')</script>";
+    echo "<script> window.location='index.php'</script>";
+}
 
 ?>
