47a55d931b9499574d5c477e6d0579e866dfd8c8
WagCar <wagner.carlin@hotmail.com>
2022-09-30 18:03:16 -0300

    arquivos ok! - Final aula 19

--- painel-adm/index.php
+++ painel-adm/index.php
@@ -1,6 +1,7 @@
 <?php
 @session_start();
 require_once("../conexao.php");
+require_once("verificar.php"); 
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -23,6 +24,7 @@ require_once("../conexao.php");
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#"> <img src="../imagens/SISFINAN.PNG" height="20PX" width="50PX"> </a>
+          
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -31,12 +33,9 @@ require_once("../conexao.php");
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
+                            Cadastros
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <li><a class="dropdown-item" href="#">Action</a></li>
@@ -52,23 +51,19 @@ require_once("../conexao.php");
                     </li>
                 </ul>
                 <div class="d-flex mr-4">
-
-                    <img class="img-profile rounded-circle" src="../imagens/usuario.png" width="40px" height="40px">                 
+                    <img class="img-profile rounded-circle" src="../imagens/usuario.png" width="40px" height="40px">   
                     
                     <div class="collapse navbar-collapse" id="navbar">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
+                                    <?php echo @$_SESSION['nome_usuario']; ?>
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">alguma Coisa</a></li>
-                                    <li><a class="dropdown-item" href="#">Outra coisa</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    <li><a class="dropdown-item" href="#">Editar Dados</a></li>
+                                    <li><hr class="dropdown-divider"> </li>
+                                    <li><a class="dropdown-item" href="../logout.php"> Sair</a></li>
                                 </ul>
                             </li>
                             <!-- PONTO PRETO PARA MELHORAR A MARGEM A DIREITA - PRECISA ARRUMAR   -->
