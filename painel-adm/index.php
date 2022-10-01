2c9a2ec9259dcd70108e0913e44d9d65481a7e7c
WagCar <wagner.carlin@hotmail.com>
2022-10-01 15:24:38 -0300

    Resolvido problema não abrir modal - nome errado

--- painel-adm/index.php
+++ painel-adm/index.php
@@ -59,7 +59,7 @@ require_once("verificar.php")
                                    <?php echo  @$_SESSION['nome_usuario'];?>
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#modaPerfil">Editar Dados</a></li>
+                                    <li><a class="dropdown-item" data-bs-toggle="modal" data-bs-target="#modalPerfil">Editar Dados</a></li>
                                     <li> <hr class="dropdown-divider"> </li>
                                     <li><a class="dropdown-item" href="../logout.php">Sair </a></li>
                                 </ul>
